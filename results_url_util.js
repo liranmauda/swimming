ce1a8394f4118f1f4d885c891719d78414f01528
liranmauda <liran.mauda@gmail.com>
2024-12-08 15:49:10 +0200

    Add the ability to get competition results from a certain date range
    Add the ability to get competition results from a certain date range
    
    Added year and date flags
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- results_url_util.js
+++ results_url_util.js
@@ -1,12 +1,74 @@
 import axios from 'axios';
+import moment from 'moment';
 import * as utils from './utils.js';
 import {
     load
 } from 'cheerio';
 
+const main_url_prefix = 'https://www.isr.org.il/'
 const url_prefix = 'https://loglig.com:2053'
 // The URL of the page
 const url = url_prefix + '/LeagueTable/AthleticsDisciplines/10358';
+const base_url = "https://www.isr.org.il/competitions.asp";
+
+// Function to extract the first row date
+function _get_first_row_date(cheerio_loaded_HTML) {
+    const date_text = cheerio_loaded_HTML('.row .c-date').first().text().trim();
+    return moment(date_text, 'D.M.YYYY').format('YYYY-MM-DD'); // Convert to standardized format
+};
+
+// Function to extract URLs from a specific date to the current date
+function get_urls_from_date(cheerio_loaded_HTML, start_date, last_date) {
+    const rows = cheerio_loaded_HTML('.row');
+    const urls = [];
+
+    rows.each((index, row) => {
+        const date_text = cheerio_loaded_HTML(row).find('.c-date').text().trim();
+        const url = cheerio_loaded_HTML(row).find('.c-name a').attr('href');
+
+        // if we are not getting last_date, we will use current date
+        if (last_date === undefined) {
+            last_date = moment();
+        }
+
+        // console.log("LMLM", start_date, "LMLM", last_date)
+        if (moment(date_text, 'D.M.YYYY').isBetween(start_date, last_date, undefined, '[]')) {
+            urls.push(main_url_prefix + url);
+        }
+    });
+
+    console.log("LMLM", urls)
+    return urls;
+};
+
+
+async function get_competition_urls(url, year, date, last_date) {
+    const url_array = [];
+    //if we are not on the base url return the url provided.
+    if (!url.includes(base_url)) {
+        url_array.push(url);
+        return url_array;
+    }
+
+    if (!url.includes('cYear')) url = url + "?cYear=" + year + "&cMonth=0&cType=1&cMode=0#searchForm";
+    console.log("LMLM", url);
+
+    const {
+        data
+    } = await axios.get(url);
+
+    const cheerio_loaded_HTML = load(data);
+    const date_text = cheerio_loaded_HTML('.row .c-date').first().text().trim();
+
+    if (date === undefined) {
+        date = moment(date_text, 'D.M.YYYY').format('YYYY-MM-DD');
+    }
+    console.log("Getting links from", year, "Start date", date);
+
+    return get_urls_from_date(cheerio_loaded_HTML, date, last_date);
+
+}
+
 
 // scrap_main_url_for_main_result_url will get a url and check if it contains the 'https://loglig.com:2053' prefix
 // if it is not it will scrap the page and return the url that contains it. 
@@ -16,7 +78,6 @@ const url = url_prefix + '/LeagueTable/AthleticsDisciplines/10358';
 // https://loglig.com:2053/LeagueTable/AthleticsDisciplines/10358 
 async function scrap_main_url_for_main_result_url(url) {
     if (url.includes(url_prefix)) return url;
-
     // Fetch the webpage content
     const {
         data
@@ -24,32 +85,36 @@ async function scrap_main_url_for_main_result_url(url) {
 
     const cheerio_loaded_HTML = load(data);
     const iframeSrc = cheerio_loaded_HTML('iframe').attr('src');
-
     return iframeSrc;
 }
 
 // scrape_main_url_for_results_links will scrap the page and search for all the results links that contains 'https://loglig.com:2053'
-async function scrape_main_url_for_results_links(url) {
+async function scrape_main_url_for_results_links(link, year, date) {
     try {
-
-        url = await scrap_main_url_for_main_result_url(url);
-
-        // Fetch the webpage content
-        const {
-            data
-        } = await axios.get(url);
-
-        const cheerio_loaded_HTML = load(data);
-
-        // Find all "תוצאות" pages from the url
         const results_links = [];
-        cheerio_loaded_HTML('a').each((index, element) => {
-            const linkText = cheerio_loaded_HTML(element).text().trim();
-            if (linkText.includes('תוצאות') && !linkText.includes('תוצאות מקצים')) {
-                const pdfUrl = cheerio_loaded_HTML(element).attr('href');
-                results_links.push(url_prefix + pdfUrl);
-            }
-        });
+        const url_array = await get_competition_urls(link, year, date);
+        for (let url of url_array) {
+            console.log("LMLM", url);
+            url = await scrap_main_url_for_main_result_url(url);
+            console.log("LMLM", url);
+            if (url === undefined) continue;
+            // Fetch the webpage content
+            const {
+                data
+            } = await axios.get(url);
+
+            const cheerio_loaded_HTML = load(data);
+
+            // Find all "תוצאות" pages from the url
+            cheerio_loaded_HTML('a').each((index, element) => {
+                const linkText = cheerio_loaded_HTML(element).text().trim();
+                if (linkText.includes('תוצאות') && !linkText.includes('תוצאות מקצים')) {
+                    const pdfUrl = cheerio_loaded_HTML(element).attr('href');
+                    results_links.push(url_prefix + pdfUrl);
+                }
+            });
+        }
+
         return results_links;
     } catch (error) {
         console.error('Error during scraping main url for results links:', error);
@@ -69,6 +134,7 @@ async function fetch_and_parse_results(url, criteria) {
         let gender;
         const event_info = cheerio_loaded_HTML('.disciplines-title h4').text().trim();
         event_info.includes("בנים") ? gender = "male" : gender = "female";
+        if (event_info === undefined) return;
         //The reason we pass event_info is for future use, if we would like to skip scrapping urls based on other criteria.
         if (should_skip_based_on_criteria(event_info, criteria)) return;
 
@@ -125,6 +191,7 @@ function should_skip_based_on_criteria(event_info, criteria) {
 }
 
 export {
+    get_competition_urls,
     scrape_main_url_for_results_links,
     fetch_and_parse_results,
 }
