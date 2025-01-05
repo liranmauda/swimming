71aac50f12411e3893bd312aef1d51067affd52c
liranmauda <liran.mauda@gmail.com>
2025-01-05 13:57:45 +0200

    Rename report.js to get_data.js
    - Rename report.js to get_data.js
    - Rename name_map to event_name_map
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

---
+++ personal-report.js
@@ -0,0 +1,7 @@
+// per event, get the first 3 places
+// per event get the first 3 times
+// Per event get the times by order
+// Do a +- range
+// Get the official times of the first/last names in each event on that year till the date of the event
+// compare with a list of predefined names (and corresponding name file)
+// Convert into a csv (can we do a tab?) 
