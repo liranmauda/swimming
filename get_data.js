3f69b3cbb2537ae8d8c082e51517e97ae9dec759
liranmauda <liran.mauda@gmail.com>
2025-01-13 10:58:37 +0200

    Moved dome functions into utils to reuse it later
    - Moved dome functions into utils to reuse it later
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- get_data.js
+++ get_data.js
@@ -58,78 +58,12 @@ const last_date = argv.last_date || moment();
 const start_date = argv.start_date; //TODO: check if we need it
 const is_output_file_name = Boolean(argv.output);
 const output_file_name = argv.output || "swimming_results.json";
+const initial_filename = argv.file_name || output_file_name;
 
 let group = argv.group;
 let year = argv.year || "2025";
-let filename = argv.file_name || output_file_name;
 
-const filters = [
-    "event",
-    "event_date",
-    "total_registrations",
-    "total_participants",
-    "age",
-    "pool_length",
-    "gender",
-    "score",
-    "time",
-    "club",
-    "birthYear",
-    "firstName",
-    "lastName",
-    "lane",
-    "heat",
-    "position",
-]
-
-// _get_data will get the data from a PDF or from a file already created
-async function _get_data(criteria) {
-    let data = [];
-    let start_date;
-    let end_date;
-    if (argv.pdf_path) {
-        const data_array = await parse_pdf.extractPDFText(pdfPath);
-        data = data.concat(parse_pdf.parseResults(data_array));
-    } else if (argv.url) {
-        const {
-            results_links,
-            from_date,
-            to_date
-        } = await parse_url.scrape_main_url_for_results_links(argv.url, year, last_date, start_date);
-        for (const element of results_links) {
-            const {
-                link,
-                event_date,
-                total_registrations,
-                total_participants
-            } = element
-            console.log("event date:", event_date, "Scrapping:", link)
-            year = utils.set_year(event_date.split(" ")[0]);
-            const to_push = await parse_url.fetch_and_parse_results(link, year, event_date.split(" ")[0], total_registrations, total_participants, criteria)
-            if (to_push) data.push(...to_push);
-            start_date = from_date;
-            end_date = to_date;
-            // break; //For debug
-        }
-    } else if (argv.file_name) {
-        const read_data = fs.readFileSync(filename, 'utf-8');
-        data = JSON.parse(read_data)
-        Object.keys(criteria).every(key =>
-            filename = filename.replace(/\.json$/, "") + "-" + String(criteria[key]).replaceAll(' ', '-') + ".json");
-    } else {
-        console.error("Data must be consumed from a file, pdf, or url");
-        usage();
-        process.exit(0);
-    }
-
-    return {
-        data,
-        start_date,
-        end_date
-    };
-}
-
-async function _set_data_file(data, append) {
+async function _set_data_file(filename, data, append) {
     if (argv.file_name && filename === output_file_name && !append) {
         console.log("Skipping writing to the same file");
         return;
@@ -155,59 +89,15 @@ async function _set_data_file(data, append) {
     }
 }
 
-
-// _construct_criteria will build the criteria to filter upon
-function _construct_criteria() {
-    const criteria = filters.reduce((criteria, filter) => {
-        if (argv[filter]) {
-            if (filter === 'position') {
-                const position = Number(argv[filter]);
-                if (!isNaN(position)) {
-                    criteria.positions = Array.from({
-                        length: position
-                    }, (_, i) => i + 1); // Create [1, 2, ..., position]
-                }
-            } else {
-                criteria[filter] = utils.reverse_string(argv[filter]);
-            }
-        }
-        return criteria;
-    }, {});
-
-    console.log(criteria);
-    return criteria;
-}
-
-// Function to filter based on dynamic keys
-function _filter_by_criteria(data, criteria) {
-    return data.filter(item => {
-        return Object.keys(criteria).every(key => {
-            if (key === 'positions') {
-                const positions = criteria[key];
-                return positions.includes(Number(item.position));
-            }
-            return String(item[key]).includes(criteria[key])
-        });
-    });
-}
-
-
 async function main() {
 
-    const criteria = _construct_criteria();
-
-    if (fs.existsSync(filename) && !append) {
-        console.log("File already exists, please use the --append flag to append to the file.")
-        process.exit(0);
-    }
     let {
         data,
         start_date,
-        end_date
-    } = await _get_data(criteria);
-    if (Object.keys(criteria).length > 0) {
-        data = _filter_by_criteria(data, criteria);
-    }
+        end_date,
+        filename
+    } = await utils.get_filtered_data(initial_filename, append);
+
     const base_file_name = filename;
     if (group) {
         data = utils.group_by_field(data, argv.group);
@@ -216,7 +106,7 @@ async function main() {
             if (!is_output_file_name) {
                 filename = base_file_name.replace(/\.json$/, "") + "-" + key.replaceAll(' ', '-') + ".json";
             }
-            await _set_data_file(data[key], append);
+            await _set_data_file(filename, data[key], append);
         }
     } else {
         console.log(data);
@@ -225,7 +115,7 @@ async function main() {
         if (!is_output_file_name && !has_date) {
             filename = filename.replace(/\.json$/, "") + "--" + start_date + "--" + end_date + ".json";
         }
-        await _set_data_file(data, append);
+        await _set_data_file(filename, data, append);
     }
 }
 
